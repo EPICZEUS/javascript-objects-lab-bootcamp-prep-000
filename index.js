b9927edd20874d667b9adfad738ad2456dd6b5f6
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const recipes = {};
 function updateObjectWithKeyAndValue(obj, k, v) {
   return Object.assign({}, obj, {[k]:v});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, k, v) {
+  obj[k] = v;
+  return obj;
+}
